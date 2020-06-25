e3c4b0559506b81ea1d75c74646c872c5a994d19
Brandon Tyler Jardine <brandon.jardine@gmail.com>
2020-06-25 18:00:27 -0500

    license info

--- synesthesia.ino
+++ synesthesia.ino
@@ -1,3 +1,25 @@
+/*
+    Synesthesia
+
+    Simple Arduino audio visualization designed for the Sparkfun spectrum
+    shield: <https://www.sparkfun.com/products/13116> and a NeoPixel strip.
+
+    © Brandon T. Jardine 2020
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
 
@@ -15,9 +37,9 @@
 #define DATA_RIGHT A1
 
 #define COLOR A2
-#define VOLUME A3
 
 #define VOLUME_ENABLE true
+#define VOLUME A3
 
 // initial base color
 #define COLOR_START 54613
@@ -50,7 +72,10 @@ void setup() {
     pinMode(DATA_LEFT, INPUT);
     pinMode(DATA_RIGHT, INPUT);
     pinMode(COLOR, INPUT);
-    pinMode(VOLUME, INPUT);
+
+    #if VOLUME_ENABLE
+        pinMode(VOLUME, INPUT);
+    #endif
 
     digitalWrite(STROBE, HIGH);
     digitalWrite(RESET, HIGH);
